0872d39ad990004798d9df1ceb0e82c6ecd90eb4
Brian Singer <brian@convolve.com>
2022-09-13 11:26:58 -0400

    Add ppt experiment

--- Attacks/PPT.py
+++ Attacks/PPT.py
@@ -1,29 +1,37 @@
-from Simulator.PowerFlowCase import PowerFlowCase
+from Simulator import PowerFlowCase, ReserveGeneration
 
 
-def ppt_attack(power_flow_case: PowerFlowCase, gens_to_turn_off):
+def ppt_attack(power_flow_case: PowerFlowCase, gens_to_turn_off, reserve_adder: ReserveGeneration):
+    power_lost = 0
+
     for gen_to_turn_off in gens_to_turn_off:
         # Turn off settings in data
         for gen in power_flow_case.generators:
-            if gen_to_turn_off == gen.id:
+            if gen_to_turn_off == gen.gen_id:
+                power_lost += gen.Pg
                 gen.status = 0
                 gen.active = False
 
         # Remove from active generators list
         for i, gen in enumerate(power_flow_case.active_generators):
-            if gen_to_turn_off == gen.id:
+            if gen_to_turn_off == gen.gen_id:
                 del power_flow_case.active_generators[i]
 
+    # Adjust generation to handle lost load
+    gen_added = reserve_adder.add_reserve_generation(power_flow_case, power_lost)
+    return
 
-def highest_ppt_attack(powerflow_case: PowerFlowCase, number_of_gens_to_turn_off, gen_offset=10):
+
+def highest_ppt_attack(powerflow_case: PowerFlowCase, reserve_adder: ReserveGeneration, number_of_gens_to_turn_off,
+                       gen_offset=10):
     # Sort generators by highest power
     sorted_gens = sorted(powerflow_case.active_generators, key=lambda gen: gen.Pg, reverse=True)
 
     # TODO ignore slack generator
     # Select generator ids
     gen_contingencies = []
-    for gen_to_turn_off in sorted_gens[gen_offset:number_of_gens_to_turn_off+gen_offset]:
-        gen_contingencies.append(gen_to_turn_off.id)
+    for gen_to_turn_off in sorted_gens[gen_offset:number_of_gens_to_turn_off + gen_offset]:
+        gen_contingencies.append(gen_to_turn_off.gen_id)
 
     # Turn them off in powerflow case
-    ppt_attack(powerflow_case, gen_contingencies)
+    ppt_attack(powerflow_case, gen_contingencies, reserve_adder)
--- Experiments/TopGeneratorsAttack.py
+++ Experiments/TopGeneratorsAttack.py
@@ -1,7 +1,6 @@
+import argparse
 import csv
 import json
-import scipy.io
-import os
 
 from Attacks import highest_ppt_attack
 from Simulator import GridSimulator, PowerFlowCase, ReserveGeneration, SimulatorResultStatus
@@ -14,39 +13,37 @@ def run_high_power_ppt(powerflow_case: PowerFlowCase, grid_simulator: GridSimula
     An offset can be supplied to start from Xth largest generator.
     """
     # Run baseline
-    grid_simulator.run_baseline()
+    grid_simulator.run_baseline(powerflow_case)
 
     grid_failed = False
     gen_offset = 10
     number_of_gens_to_turnoff = 1
-    excess_slack = 0
     required_gens = 1
     while not grid_failed:
         # Turn off highest generator (modification is persistent so number_of_gens_to_turnoff should always be 1
-        highest_ppt_attack(powerflow_case, number_of_gens_to_turnoff, gen_offset=gen_offset)
+        highest_ppt_attack(powerflow_case, reserve_adder, number_of_gens_to_turnoff, gen_offset=gen_offset)
 
         # Run simulation
-        sim_state, num_component_failed, res = grid_simulator.run_simulation()
-        # TODO
-        excess_slack = res.excess_slack_P
+        sim_state = grid_simulator.run_simulation(powerflow_case, reserve_adder)
 
         if sim_state != SimulatorResultStatus.SUCCESS:
             grid_failed = True
         required_gens += 1
 
+    required_gens -= 1
     print(f'Total Gens Required for Failure: {required_gens}')
 
     return required_gens, sim_state
 
 
-def ppt_with_varying_droop(config, droop_start=0, droop_step=.01, droop_max=.5):
+def ppt_with_varying_droop(config, droop_start=0, droop_step=.1, droop_max=.5):
     # Load topology
     powerflow_case = PowerFlowCase(matpower_path=config['topology'])
 
     # Setup Simulator
     grid_simulator = GridSimulator(powerflow_case)
     reserve_adder = ReserveGeneration(powerflow_case, config['total_agc_reserves'])
-    grid_simulator.run_baseline()
+    grid_simulator.run_baseline(powerflow_case)
 
     # Keep adding more droop
     results = []
@@ -55,6 +52,7 @@ def ppt_with_varying_droop(config, droop_start=0, droop_step=.01, droop_max=.5):
         powerflow_case = PowerFlowCase(matpower_path=config['topology'])
         # Adjust droop limit
         grid_simulator.droop_limit = droop_start
+        reserve_adder.droop_ramp = droop_start
         # Rerun simulation
         required_gens, final_sim_state = run_high_power_ppt(powerflow_case, grid_simulator, reserve_adder)
         results.append([droop_start, required_gens, final_sim_state])
@@ -70,4 +68,13 @@ def ppt_with_varying_droop(config, droop_start=0, droop_step=.01, droop_max=.5):
         for res in results:
             csvwriter.writerow(res)
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config_file', help='Specify path of configuration file')
+    args = parser.parse_args()
 
+    # Read config file
+    config_file = open(args.config_file)
+    config = json.load(config_file)
+
+    ppt_with_varying_droop(config)
